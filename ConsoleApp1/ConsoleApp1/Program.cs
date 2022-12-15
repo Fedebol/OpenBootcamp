0cbb4a6695068580a10dc43f0d3266bbfbb3291d
fedebol <fedeboldrini@gamil.com>
2022-12-15 11:17:00 -0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿class program
+{
+
+    public static void Main(string[] args)
+    {
+        var a = 1;
+        var b = 2;
+        var c = 3;
+
+        int resultado = suma(a, b, c);
+        coche coche = new coche();
+        coche.sumarPuerta();
+        Console.WriteLine(coche.puertas);
+
+    }
+
+    public static int suma(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+
+    public class coche
+    {
+        public int puertas = 4;
+        public void sumarPuerta()
+        {
+            this.puertas++;
+        }
+    }
+
+
+
+}

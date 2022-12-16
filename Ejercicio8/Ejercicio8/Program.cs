4175722c168df47a77ae565dd3e26dd97921c91b
fedebol <fedeboldrini@gamil.com>
2022-12-16 09:02:38 -0300

    ejercicio 8

---
+++ Ejercicio8/Ejercicio8/Program.cs
@@ -0,0 +1,17 @@
+﻿Persona persona1 = new Persona();
+persona1.Nombre = "Federico";
+persona1.Edad = 34;
+persona1.Telefono = 088840052;
+
+Console.WriteLine(persona1.Nombre);
+Console.WriteLine(persona1.Telefono);
+Console.WriteLine(persona1.Edad);
+class Persona
+  {
+   public string Nombre { get; set; }
+   public int Telefono { get; set; }
+   public int Edad { get; set; }
+
+  }
+
+

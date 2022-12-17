0d229aac574143945d0d3bab0f81617429933df7
fedebol <fedeboldrini@gamil.com>
2022-12-17 16:13:35 -0300

    ejercicio1 csharp

---
+++ ejercicio1csharp/ejercicio1csharp/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+Console.WriteLine("ingrese su nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("su nombre es: " + nombre);
+
+var hora = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine("la hora actual es: " + hora);

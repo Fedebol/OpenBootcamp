3ce6385e3df5f77c14d8774ff9fac3ee785fdcf0
fedebol <fedeboldrini@gamil.com>
2022-12-17 23:00:13 -0300

    ejercicio 4 csharp

---
+++ Ejercicio4csharp/Ejercicio4csharp/Program.cs
@@ -0,0 +1,82 @@
+﻿//while
+Console.WriteLine("ingrese un numero: ");
+var numero = Convert.ToInt32(Console.ReadLine());
+var j = 1;
+while(j <= 10)
+{
+    Console.WriteLine(numero*j);
+    j++;
+}
+
+//do while
+var estado = "n";
+do
+{
+
+Console.WriteLine("ingrese un numero: ");
+var numeroDo = Convert.ToInt32(Console.ReadLine());
+    if (numeroDo == 0)
+    {
+        Console.WriteLine("el numero es cero");
+    }
+    else if(numeroDo > 0)
+    {
+        Console.WriteLine("el numero es positivo");
+    }
+    else
+    {
+        Console.WriteLine("el numero es negativo");
+    }
+    Console.WriteLine("desea ingresar otro numero? s/n ");
+    estado = Console.ReadLine();
+}while(estado == "s");
+//for
+
+Console.WriteLine("ingrese un numero para el ancho: ");
+var ancho = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ingrese un numero para el alto: ");
+var alto = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("desea que la figura este rellena? s/n: ");
+var relleno = Console.ReadLine();
+
+for(var i = 0; i < alto; i++)
+{
+    Console.WriteLine();
+    for (var k = 0; k < ancho; k++)
+    {
+        if (i == 0)
+        {
+            Console.Write("*");
+        }
+        else if (i == alto-1)
+        {
+            Console.Write('*');
+        }
+        else
+        {
+            if (k == 0)
+            {
+                Console.Write("*");
+            }
+            else if (k == ancho - 1)
+            {
+                Console.Write('*');
+            }
+            else
+            {
+
+                if (relleno == "s")
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+        
+
+    }
+}
+

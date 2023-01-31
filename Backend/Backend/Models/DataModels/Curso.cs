8efc081faff8bc9f48a59c06cd4e94165b3f37f1
fedebol <fedeboldrini@gamil.com>
2023-01-31 14:57:52 -0300

    backend ejercicio 1

---
+++ Backend/Backend/Models/DataModels/Curso.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Backend.Models.DataModels
+{
+
+    public enum Nivel
+    {
+        Basico,
+        Intermedio,
+        Avanzado
+    }
+
+    public class Curso : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Nombre { get; set; } = string.Empty;
+        [Required, StringLength(280)]
+        public string DescripcionCorta { get; set; } = string.Empty;
+        [Required]
+        public string DescripcionLarga { get; set; } = string.Empty;
+        [Required]
+        public string PublicoObjetivo { get; set; } = string.Empty;
+        [Required]
+        public string Objetivos { get; set; } = string.Empty;
+        [Required]
+        public string Requisitos { get; set; } = string.Empty;
+        [Required]
+        public Nivel Nivel { get; set; } = Nivel.Basico;
+
+    }
+}
